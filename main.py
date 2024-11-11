7167d0ac581692944ecd69e0804730a69b5704cb
yonatan <yonatanalebachew7@gmail.com>
2024-11-11 14:31:26 +0300

    main python added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telegram_bot.bot import DictionaryBot
 
 def main():
     print("Starting ...")
-    dictionary_bot = DictionaryBot()  # Initialize the Telegram bot
+    dictionary_bot = DictionaryBot()
     dictionary_bot.run()
 
 if __name__ == "__main__":
